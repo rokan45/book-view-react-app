9a03026d37a6e5ab2ea008f455314184e826a7ed
rokan45 <rokanuzzaman4567@gmail.com>
2026-04-08 21:15:52 +0600

    navbar and homepage added with routing'

---
+++ src/componet/bookpage/Bookpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bookpage = () => {
+    return (
+        <div>
+            <h1>This is book page</h1>
+        </div>
+    );
+};
+
+export default Bookpage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router'
 import Root from './componet/Root/Root.jsx'
+import Navbar from './componet/navbar/Navbar.jsx'
+import Bookpage from './componet/bookpage/Bookpage.jsx'
+import Homepage from './componet/homepage/Homepage.jsx'
+
 
 
 const router=createBrowserRouter([
   {path:'/',
     element:<Root/>,
+    children:[
+      {index:true,element:<Homepage/>},
+      {
+        path:"books",
+        element:<Bookpage/>
+      }
+    ]
     
   }
 ]);
